b0f879034aa6c58e9506e186587a2c2725bd1161
Provonsal <alecsw86@gmail.com>
2025-09-30 22:23:09 +0700

    refactor: Changed and refactored code of tests/time_comparing.py for more details and information

--- tests/test_time_comparing.py
+++ tests/test_time_comparing.py
@@ -1,4 +1,3 @@
-import line_profiler
 from autoproperty import AutoProperty
 from autoproperty import LightAutoProperty
 import timeit
@@ -6,23 +5,6 @@ import timeit
 AutoProperty.validate_fields = False
 
 def time_comparing():
-    
-    class Descriptor():
-        value: int
-
-        def __init__(self, value) -> None:
-            self.value = value
-
-        def __set__(self, instance, obj):
-            self.value = obj
-
-        def __get__(self, instance, owner=None):
-            
-            # If instance is not exist then return class type
-            if instance is None:
-                return self #type: ignore
-            
-            return self.value
         
     class A():
 
@@ -45,63 +27,65 @@ def time_comparing():
         def __init__(self, x, y, z) -> None:
             self.X = x
             self.Y = y
-            self.Z = Descriptor(z)
-
-    
 
     obj = A(3,3,3)
 
-    #@line_profiler.profile
     def autoproperty_get():
         obj.X
 
-    #@line_profiler.profile
     def light_autoproperty_get():
         obj.G
         
-    
     def basic_property_get():
         obj.Y
         
-    #@line_profiler.profile
     def autoproperty_set():
         obj.X = 2
 
-    #@line_profiler.profile
     def light_autoproperty_set():
         obj.G = 2
         
     def basic_property_set():
         obj.Y = 2
 
-    execution_time_autoproperty_write   = timeit.timeit(autoproperty_set, number=1_000_000)
-    execution_time_autoproperty         = timeit.timeit(autoproperty_get, number=1_000_000)
-    execution_time_light_property_write = timeit.timeit(light_autoproperty_set, number=1_000_000)
-    execution_time_light_property       = timeit.timeit(light_autoproperty_get, number=1_000_000)
-    execution_time_basic_property_write = timeit.timeit(basic_property_set, number=1_000_000)
-    execution_time_basic_property       = timeit.timeit(basic_property_get, number=1_000_000)
+    try_count = 1_000_000_000
 
-    print("autoproperty time: ", execution_time_autoproperty)
+    execution_time_autoproperty         = timeit.timeit(autoproperty_get, number=try_count)
+    execution_time_light_property       = timeit.timeit(light_autoproperty_get, number=try_count)
+    execution_time_basic_property       = timeit.timeit(basic_property_get, number=try_count)
+    
+    execution_time_autoproperty_write   = timeit.timeit(autoproperty_set, number=try_count)
+    execution_time_light_property_write = timeit.timeit(light_autoproperty_set, number=try_count)
+    execution_time_basic_property_write = timeit.timeit(basic_property_set, number=try_count)
+
+    print("autoproperty getter time: ", execution_time_autoproperty)
     print("autoproperty setter time: ", execution_time_autoproperty_write)
-    print("light autoproperty time: ", execution_time_light_property)
+    print()
+    print("light autoproperty getter time: ", execution_time_light_property)
     print("light autoproperty setter time: ", execution_time_light_property_write)
-    print("basic property", execution_time_basic_property)
-    print("basic property write", execution_time_basic_property_write)
-    print("diff 1", execution_time_autoproperty/execution_time_light_property)
-    print("diff 2", execution_time_autoproperty_write/execution_time_light_property_write)
-    #print(A.X._get_debug_cache_info())
-
-
+    print()
+    print("basic property getter time", execution_time_basic_property)
+    print("basic property setter time", execution_time_basic_property_write)
+    print()
+    print("diff between getters of autoproperty and it light version", execution_time_autoproperty/execution_time_light_property)
+    print("diff between setters of autoproperty and it light version", execution_time_autoproperty_write/execution_time_light_property_write)
+    print()
+    print("diff between getters of autoproperty and basic solution", execution_time_autoproperty/execution_time_basic_property)
+    print("diff between setters of autoproperty and basic solution", execution_time_autoproperty_write/execution_time_basic_property_write)
+    
+    light_autoproperty_percent_getter = (((execution_time_autoproperty-execution_time_light_property)/execution_time_light_property)*100)+100
+    basic_property_percent_getter = (((execution_time_autoproperty-execution_time_basic_property)/execution_time_basic_property)*100)+100
+    
+    light_autoproperty_percent_setter = (((execution_time_autoproperty_write-execution_time_light_property_write)/execution_time_light_property_write)*100)+100
+    basic_property_percent_setter = (((execution_time_autoproperty_write-execution_time_basic_property_write)/execution_time_basic_property_write)*100)+100
+    
+    print()
+    print("Lets find out who is faster. More percent - faster")
+    print("getters")
+    print(f"If AutoProperty result = 100%, then LightAutoProperty = {light_autoproperty_percent_getter:.0f}%, and basic property = {basic_property_percent_getter:.0f}%")
+    print()
+    print("setters")
+    print(f"If AutoProperty result = 100%, then LightAutoProperty = {light_autoproperty_percent_setter:.0f}%, and basic property = {basic_property_percent_setter:.0f}%")
+    
 
 time_comparing()
-
-
-
-
-
-"""
-1 try
-autoproperty time:  0.15369665699836332
-property time:  0.048774095994303934
-diff 3.151194376135905
-"""
